58f83e63a2fc2cf08c6e4c1c8aa1037fdaf16f78
Quoc-Bao-213 <taquocbao.it.99@gmail.com>
2023-03-24 15:58:19 +0700

    Project finished

--- package/config/app.go
+++ package/config/app.go
@@ -12,7 +12,7 @@ var (
 // Sử dụng `gorm.Open để kết nối db`.
 // Database kết nối tới trong package là "bookstore" với user: "root", password rỗng, utf8 charset, parseTime là true và location được đặt ở local.
 func Connect() {
-	d, err := gorm.Open("mysql", "root:/bookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:@/bookstore?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil { // nil == null
 		panic(err) // panic: ngừng thực hiện chương trình ngay lập tức và thông báo lỗi
--- package/utils/utils.go
+++ package/utils/utils.go
@@ -1,14 +1,16 @@
 package utils
 
 import (
-	"encode/json"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 )
 
+// Nhận tham số là một đối tượng request `*http.Request` và một biến `X` có kiểu interface{}.
+// Hàm được sử dụng để phân tích nội dung của request, đọc dữ liệu từ ngoài vào, phân tích nội dung đó từ JSON và gán cho một object X tương ứng.
 func ParseBody(r *http.Request, X interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), X); err != nil {
+	if body, err := ioutil.ReadAll(r.Body); err == nil { // Đọc request body `ioutil.ReadAll(r.Body)`, `r` là một instance của `http.Request`.
+		if err := json.Unmarshal([]byte(body), X); err != nil { // Hàm thực hiện phân tích JSON và gán giá trị cho biến `X` đối với dữ liệu vừa được đọc
 			return
 		}
 	}
